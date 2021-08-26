064285b36fc12da248c1380ed026024ca376f5cc
Abhishek <abhishek.anand@mtap.in>
2021-08-26 15:54:55 +0530

    done

--- todo.js
+++ todo.js
@@ -1,14 +1,19 @@
-let tasks = [];
-const tasksList = document.getElementById("list");
-const addTaskInputBox = document.getElementById("add-task");
+let tasks = [[], [], []];  //Array of task
+let currIndex = 0;
 
-function addTodo(task) {
+const tasksList = document.getElementById("list");  //grab the list
+const addTaskInputBox = document.getElementById("Add-task"); //we have to select the input box with id add-task
+
+
+
+
+function addTodo(task) {  //function to add todo list
 
   let flag = false;
-  tasks.forEach(t => {
+  tasks[currIndex].forEach(t => {
 
-    console.log(t.text.includes(task.text));
-    if (t.text.includes(task.text)) {
+    console.log(t.text === task.text);
+    if (t.text === task.text) {
 
       flag = true;
     }
@@ -18,51 +23,68 @@ function addTodo(task) {
     alert("already in the list")
     return;
   }
-  tasks.push(task);
+  tasks[currIndex].push(task); //adding stuff to the tasks array
 
   renderList();
 }
 
-function deleteTodo(taskId) {
-  const newTasks = tasks.filter(function (task) {
-    return task.id !== taskId;
+function deleteTodo(taskId) { //function to delete todo list it will basically delete the items and taskId here is differentiater
+  const newTasks = tasks[currIndex].filter(function (task) {//here task.filter will give new array after filtering
+    return task.id !== taskId; //get all the task wich not equal to taskId 
   });
 
-  tasks = newTasks;
+  tasks[currIndex] = newTasks;
   renderList();
 }
-
+//var li;
 function renderList() {
   tasksList.innerHTML = '';
-  for (let i = 0; i < tasks.length; i++) {
-    const li = document.createElement('li');
-    const task = tasks[i];
+  for (let i = 0; i < tasks[currIndex].length; i++) {
+    const li = document.createElement('li');//here we are creating the element using li tag and pushin it to the dom
+    const task = tasks[currIndex][i];
 
-    li.innerHTML = `
-     <input type="checkbox" id="${task.id}" />
+    li.innerHTML = `  
+     <input type="checkbox" id="${task.id}" class="check" />
      <label for="${task.id}">${task.text}</label>
-     <button data-taskId="${task.id}" data-test="test" class="select">Select</button>
+     <button data-taskId="${task.id}" data-test="test" class="select">Delete</button>
+
+     <br />
+     
    `;
     tasksList.appendChild(li);
-  }
-}
-function checkTodo(taskId) {
-  const taskIndex = tasks.findIndex(function (task) {
-    return task.id === taskId
-  });
+    li.addEventListener('click', function () {
+      li.style.textDecoration = "line-through";
+    })
 
-  tasks[taskIndex].done = !tasks[taskIndex].done;
+  }
+
 }
+
+
+let selection = document.querySelector('select');
+
+
+selection.addEventListener('change', () => {
+  currIndex = Number(selection.value);
+  renderList();
+})
+
 
 function initialize() {
 
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Delete' || e.key === 'Backspace') {
-      const taskid = Number(e.target.dataset.taskid);
+      const taskid = Number(e.target.dataset.taskid);//grab it in taskid
+
 
       deleteTodo(taskid);
     }
+
   });
+
+  //document.addEventListener('click', handleClick);
+
+
 }
 document.getElementById('Add-task').addEventListener('keydown', function (evt) {
   const text = evt.target.value;
@@ -77,6 +99,12 @@ document.getElementById('Add-task').addEventListener('keydown', function (evt) {
     addTodo(task);
 
     renderList();
+
+    addTaskInputBox.value = "";
+    //checkTodo();
   }
+
 });
+
+
 initialize();
